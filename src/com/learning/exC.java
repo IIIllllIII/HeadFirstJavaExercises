bdf088c08a79aa27b6021b531c82a6573f37062b
Andrei Paraschiv <paraschi@adobe.com>
2017-02-16 22:57:23 +0200

    Ex C - be the compiler

---
+++ src/com/learning/exC.java
@@ -0,0 +1,16 @@
+package com.learning;
+
+/**
+ * @version JavaLearning, 02/16/2017
+ */
+public class exC {
+    public static void main (String[] args){
+        int x = 5;
+        while (x > 1) {
+            x = x - 1;
+            if (x < 3) {
+                System.out.println("a smaller number is : " + x);
+            }
+        }
+    }
+}
