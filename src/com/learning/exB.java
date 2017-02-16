e252ff626df41c4bdcb543375c43e28ff0bf671b
Andrei Paraschiv <paraschi@adobe.com>
2017-02-16 22:56:25 +0200

    Ex B - be the compiler

---
+++ src/com/learning/exB.java
@@ -0,0 +1,16 @@
+package com.learning;
+
+/**
+ * @version JavaLearning, 02/16/2017
+ */
+public class exB {
+    public static void main (String [] args){
+        int x = 5;
+        while (x > 1) {
+            x = x - 1;
+            if (x < 3) {
+                System.out.println(" the number is : " + x );
+            }
+        }
+    }
+}
