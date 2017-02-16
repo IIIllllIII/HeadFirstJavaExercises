a27d137ea80a88d03b946f4e8b65a6b1773e3066
Andrei Paraschiv <paraschi@adobe.com>
2017-02-16 22:53:38 +0200

    Ex A - be the compiler

---
+++ src/com/learning/exA.java
@@ -0,0 +1,16 @@
+package com.learning;
+
+/**
+ * @version JavaLearning, 02/16/2017
+ */
+public class exA {
+    public static void main (String [] args){
+        int x = 1;
+        while (x < 10) {
+            x = x + 3;
+            if (x > 3) {
+                System.out.println("the number is : " + x );
+            }
+        }
+    }
+}
